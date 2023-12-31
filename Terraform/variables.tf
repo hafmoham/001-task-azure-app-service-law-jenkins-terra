6a4ca917413b093fb0511552d9a8343a4beffc7d
HM <95107265+hafmoham@users.noreply.github.com>
2023-12-31 15:14:46 -0500

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -18,17 +18,17 @@ variable "deploy_location" {
 
 variable "rg_shared_name" {
   type        = string
-  default     = "rg-shared-resources-moha"
+  default     = "rg-shared-resources-vterra"
 }
 
 variable "webappname" {
   type        = string
-  default     = "webapp-134rt4-moha"
+  default     = "webapp-134rt4-vterra"
 }
 
 variable "workspacename" {
   type        = string
-  default     = "myworkspace13456moha"
+  default     = "vterraworkspace13456"
 }
 variable "tenant_id" {
   type        = string
