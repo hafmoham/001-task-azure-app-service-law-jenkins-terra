d6a2c8cc74e391a596aaac194ea26fecacc7804a
HM <95107265+hafmoham@users.noreply.github.com>
2023-12-31 15:02:14 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,13 +20,8 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  # sku_name            = "B1"
-reserved = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  sku_name            = "B1"
+  reserved = true
 
   tags = {
     owner = var.owner
