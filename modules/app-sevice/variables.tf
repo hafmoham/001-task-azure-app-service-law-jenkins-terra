ac5fb040f7ad1c5949f03c5040fdce2b5a9a38ab
HM <hafeez.itm@gmail.com>
2024-01-01 11:22:01 -0500

    created a copy of variables.tf inside app-service folder

---
+++ modules/app-sevice/variables.tf
@@ -0,0 +1,46 @@
+variable "owner" {
+  default = ""
+  description = "Please enter the owner"
+  type = string
+}
+
+variable "environment" {
+  type = string
+  description = "Please specify the environment"
+  default = ""
+
+}
+
+variable "deploy_location" {
+  type        = string
+  default     = "eastus"
+}
+
+variable "resourcegroupname" {
+  type        = string
+  default     = "rg-vterramodules"
+}
+
+
+variable appserviceplanname{
+  type        = string
+  default     = "appservplan-vterramodules"
+}
+
+variable "webappname" {
+  type        = string
+  default     = "webapp-vterramodules"
+}
+
+variable "workspacename" {
+  type        = string
+  default     = "law-vterramodules"
+}
+variable "tenant_id" {
+  type        = string
+  default     = "11111111-1111-1111-1111-111111111111"
+}
+variable "subscription_id" {
+  type        = string
+  default     = "00000000-0000-0000-0000-000000000000"
+}
